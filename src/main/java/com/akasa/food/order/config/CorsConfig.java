8644b19119d81eb73fdcf518180de9999d121e83
abhinath-jadhav <137531738+abhinath-jadhav@users.noreply.github.com>
2024-08-16 16:30:57 +0530

    updatd cors

--- src/main/java/com/akasa/food/order/config/CorsConfig.java
+++ src/main/java/com/akasa/food/order/config/CorsConfig.java
@@ -1,20 +1,18 @@
-// src/main/java/com/example/YourApplicationConfig.java
 package com.akasa.food.order.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class YourApplicationConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS configuration to all endpoints
-                .allowedOrigins("http://localhost:5173/") // Allow your front-end URL
+                .allowedOrigins("*") // Allow your front-end URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type") // Allow custom headers
-                .allowCredentials(true); // Allow credentials (cookies, HTTP authentication)
+                .allowCredentials(false); // Allow credentials (cookies, HTTP authentication)
     }
 }
