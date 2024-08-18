cece9d319a04caa0af366495d3cba3567c5a757f
abhinath-jadhav <137531738+abhinath-jadhav@users.noreply.github.com>
2024-08-18 11:32:42 +0530

    Loader updated

--- akasa-react/src/Components/Loading.jsx
+++ akasa-react/src/Components/Loading.jsx
@@ -4,29 +4,24 @@ import SubFooter from "./SubFooter";
 
 const Loading = () => {
   return (
-    <>
-      <div className="h-[70vh] flex items-center justify-center">
-        <Container className={"w-full"}>
-          <div className="text-center">
-            {/* Ping Animation */}
-            <div className="flex justify-center items-center">
-              <div className="rounded-full h-20 w-20 bg-secondary animate-ping"></div>
-            </div>
-
-            {/* Typing Animation */}
-            <div className="mt-10">
-              <p className="text-2xl text-secondary whitespace-nowrap overflow-hidden border-r-4 border-secondary animate-typing">
-                Loading data...
-                <span className="animate-blink">|</span>
-              </p>
-            </div>
+    <div className="h-[80vh] flex items-center justify-center">
+      <Container className={"w-full"}>
+        <div className="text-center">
+          {/* Ping Animation */}
+          <div className="flex justify-center items-center">
+            <div className="rounded-full h-20 w-20 bg-secondary animate-ping"></div>
           </div>
-        </Container>
-      </div>
-      <div>
-        <SubFooter button={"Home"} desc={"Go to home page ??"} to={"/"} />
-      </div>
-    </>
+
+          {/* Typing Animation */}
+          <div className="mt-10">
+            <p className="text-2xl text-secondary whitespace-nowrap overflow-hidden border-r-4 border-secondary animate-typing">
+              Loading data...
+              <span className="animate-blink">|</span>
+            </p>
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 };
 
