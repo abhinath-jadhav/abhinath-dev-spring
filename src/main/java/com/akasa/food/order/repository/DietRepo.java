c0906b24418d63b096dcdee83715441c75e7f5f8
abhinath.jadhav <abhinath.jadhav18@gmail.com>
2024-12-09 13:15:19 +0530

    updated dev db

--- src/main/java/com/akasa/food/order/repository/DietRepo.java
+++
@@ -1,9 +0,0 @@
-package com.akasa.food.order.repository;
-
-import com.akasa.food.order.models.Diet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DietRepo extends JpaRepository<Diet, Long> {
-}
